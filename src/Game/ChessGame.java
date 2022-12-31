a9514719dd129bffc2aa8befda13880e07fda3be
Erekir <115874090+Erekir-1234@users.noreply.github.com>
2022-12-31 10:52:51 +0530

    Update ChessGame.java

--- src/Game/ChessGame.java
+++ src/Game/ChessGame.java
@@ -2,13 +2,14 @@ package Game;
 
 import javax.swing.*;
 
-public class ChessGame {
-    
-    //The Board
-    //smallcase means black
-    //uppercase means white
+public class ChessGame{
 
-    static String board[][] = new String[][]{    
+    static String board[][] = new String[][]{ 
+        
+        //Visual representation of a board
+        // smallcase = black
+        // uppercase = white
+        
             {"r", "n", "b", "q", "k", "b", "n", "r"},
             {"p", "p", "p", "p", "p", "p", "p", "p"},
             {" ", " ", " ", " ", " ", " ", " ", " "},
@@ -32,11 +33,23 @@ public class ChessGame {
         jf.add(gui);
         jf.setSize(475, 500); //screen size
         jf.setVisible(true); // makes the screen visisble
-        jf.setResizable(false); //does not allow the screen to be resized
+        jf.setResizable(true); //does not allow the screen to be resized
         jf.setLocationRelativeTo(null); //centres the screen to the middle
         jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); //makes sure that the window is closed when you press the 'x' 
    
-       
+        while(!"K".equals(board[whiteKing / 8] [whiteKing % 8])){ //a thing check whether the king is in check or not
+            whiteKing++;
+        }
+        
+        while(!"k".equals(board[blackKing / 8] [blackKing % 8])){
+            blackKing++;
+        }
+        
+        private static moveMade(){  //checks whose move it is
+            moves++;
+        }
+        
+        
     }
 }
 
